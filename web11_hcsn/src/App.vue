162ded02734bb5c5eec79c35834896d901b91a2a
Dat Ngo <ngodat02092001@gmail.com>
2022-11-29 22:37:53 +0700

    "base project"

---
+++ web11_hcsn/src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+<div class="container">
+  <div class="sidebar">sidebar</div>
+  <div class="main">main</div>
+</div>
+</template>
+
+<style scoped>
+@import url(./css/index.css);
+</style>
